04dc4437dff6fceef050739458008a3f363805cb
shubham <maskeshubham555@gmail.com>
2024-07-08 17:43:08 +0530

    Implemented jarvis analyze section,why jv section,Serve section , partners section and collaborators ,media section with

--- Assets/index.js
+++ Assets/index.js
@@ -1,9 +1,9 @@
+// dropdown functionatility code Started
 let visibility = false;
 
 function productList() {
     var productDropdown = document.getElementById("product-dropdown");
     if(!visibility){
-        // console.log("hi")
         productDropdown.style.display = "block";
         visibility = true;
     }else if(visibility = true){
@@ -26,3 +26,25 @@ function solutionList() {
     }
     return
 }
+
+// dropdown functionatility code end
+
+// auto-Slider code started
+
+let count = 1;
+const removebg = ()=>{
+    let radioBtn = document.getElementById('radio-btn'+(count))
+    radioBtn.style.backgroundColor = "#eef4fa"
+}
+
+setInterval(function(){
+    let radioBtn = document.getElementById('radio-btn'+(count))
+    radioBtn.style.backgroundColor = "#1566d6";
+    document.getElementById('btn'+count).checked = true;
+    count++
+    if(count > 2){
+        count = 1;
+    }
+    removebg()
+}, 5000)
+// auto-Slider code end
